9322e6620af2d39411b03f95ace38a7afa19436a
Виталий Сухих <suhih@VSPC34.RU>
2022-02-24 15:47:44 +0300

    Добавьте файлы проекта.

---
+++ Generics/IntNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class IntNumber
+    {
+        private int d;
+        public IntNumber(int _d)
+        {
+            d = _d;
+        }
+
+        public int Number
+        {
+            get
+            {
+                return d;
+            }
+
+            set
+            {
+                d = value;
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("d = {0}", d);
+        }
+    }
+}
---
+++ Generics/Program2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntNumber number = new IntNumber(5);
+
+            number.Display();
+
+            number.Number = 25;
+            number.Display();
+
+            int t;
+            t = number.Number;
+            Console.WriteLine("t = {0}", t);
+        }
+    }
+}
